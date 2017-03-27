10963a45492b996dbdf8a5690e18f122105de735
David Zhu <hdavidzhu@gmail.com>
2017-03-27 11:12:24 -0400

    Updated training scripts.

--- food_101.sh
+++ food_101.sh
@@ -1,7 +1,8 @@
 python $HOME/tensorflow/tensorflow/examples/image_retraining/retrain.py \
---bottleneck_dir=$HOME/data/models/food_101/bottlenecks \
---how_many_training_steps 5000 \
---model_dir=$HOME/data/models/food_101/inception \
+--image_dir=$HOME/data/food_101/images \
 --output_graph=$HOME/data/models/food_101/retrained_graph.pb \
 --output_labels=$HOME/data/models/food_101/retrained_labels.txt \
---image_dir $HOME/data/food_101/images
+--summaries_dir=$HOME/data/models/food_101/retrain_logs \
+--how_many_training_steps=5000 \
+--model_dir=$HOME/data/models/food_101/inception \
+--bottleneck_dir=$HOME/data/models/food_101/bottlenecks \
---
+++ food_101_v3.sh
@@ -0,0 +1,9 @@
+python $HOME/tensorflow/tensorflow/examples/image_retraining/retrain.py \
+--image_dir=$HOME/data/food_101/images \
+--output_graph=$HOME/data/models/food_101_v3/retrained_graph.pb \
+--output_labels=$HOME/data/models/food_101_v3/retrained_labels.txt \
+--summaries_dir=$HOME/data/models/food_101_v3/retrain_logs \
+--how_many_training_steps=20000 \
+--learning_rate=0.005 \
+--model_dir=$HOME/data/models/food_101_v3/inception \
+--bottleneck_dir=$HOME/data/models/food_101/bottlenecks \
