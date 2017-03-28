f3a5c83b80c7b37277f59538f5b47cb4dfab7162
David Zhu <hdavidzhu@gmail.com>
2017-03-28 15:34:20 -0400

    Added v4 for food 101.
    Moved out retraining logs so we can push to git.
    
    Removed large retraining datasets from git log.

--- training/food_101.v04.sh
+++ training/food_101.v04.sh
@@ -2,7 +2,7 @@ python $HOME/tensorflow/tensorflow/examples/image_retraining/retrain.py \
 --image_dir=$HOME/data/food_101/images \
 --output_graph=$HOME/dcnns/models/food_101_v4/retrained_graph.pb \
 --output_labels=$HOME/dcnns/models/food_101_v4/retrained_labels.txt \
---summaries_dir=$HOME/dcnns/models/food_101_v4/retrain_logs \
+--summaries_dir=$HOME/data/models/food_101_v4/retrain_logs \
 --how_many_training_steps=250000 \
 --learning_rate=0.01 \
 --model_dir=$HOME/data/models/food_101_v4/inception \
